c052741d5e064ded450dd760bdd0ac1661d3b05b
surgvimon <surgvimon@gmail.com>
2023-05-04 18:16:33 +0700

    complete the forgot password

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import OAuth from './components/OAuth';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import {toast} from "react-toastify";
 
 export default function ForgotPassword() {
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
   function onChange(e) {
     setEmail(e.target.value);
+  }
+  async function onSubmit(e){
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was send.")
+    } catch (error) {
+      toast.error("Cloud not sent reset password.");
+    }
   }
   return (
     <section>
@@ -16,7 +28,7 @@ export default function ForgotPassword() {
           <img src='https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80' alt="key" className='w-full rounded-2xl'/>
         </div>
         <div  className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <div>
               <input
                 className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6' 
