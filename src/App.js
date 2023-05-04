9a933f3cf4873f5f9162b4726910a077e2795824
surgvimon <surgvimon@gmail.com>
2023-05-04 15:42:07 +0700

    install firebase and toastify and sign up user

--- src/App.js
+++ src/App.js
@@ -6,22 +6,37 @@ import Offers from "./pages/Offers"
 import Profile from "./pages/Profile"
 import ForgotPassword from "./pages/ForgotPassword"
 import Header from "./pages/components/Header";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
     
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Sign-up" element={ <SignUp />} />
-        <Route path="/Sign-in" element={ <SignIn />} />
-        <Route path="/Offers" element={ <Offers />} />
-        <Route path="/profile" element={ <Profile />} />
-        <Route path="/forgot-password" element={ <ForgotPassword />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Sign-up" element={ <SignUp />} />
+          <Route path="/Sign-in" element={ <SignIn />} />
+          <Route path="/Offers" element={ <Offers />} />
+          <Route path="/profile" element={ <Profile />} />
+          <Route path="/forgot-password" element={ <ForgotPassword />} />
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+
     </>
   );
 }
