ef9ae8f62257f7cbdc7bcb2541cde41631d394a5
surgvimon <surgvimon@gmail.com>
2023-05-04 23:20:37 +0700

    add and edit funtionality to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
 import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from '../firebase';
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changDetail, setChangDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -14,16 +18,47 @@ export default function Profile() {
     auth.signOut();
     navigate('/');
   }
+  function onChange(e) {
+    setFormData((preState) => ({
+      ...preState,
+      [e.target.id]: e.target.value
+    }))
+  }
+  async function onSubmit(){
+    try {
+      if(auth.currentUser.displayName !== name){
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+        toast.success("Profile detail update.")
+      }
+      
+    } catch (error) {
+      toast.error("Cloud not update the profile detail.")
+    }
+
+  }
   return (
     <>
     <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
       <h1 className='text-3xl text-center mt-6 font-bold'>Profile Page</h1>
       <div className='w-full md:w-[50%] m-auto mt-6 px-3'>
         <form>
-          <input type="text" id="name" value={name} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out" />
+          <input 
+          type="text" 
+          id="name" 
+          value={name} 
+          disabled={!changDetail} 
+          onChange={onChange}
+            className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changDetail && "bg-red-200 focus:bg-red-200"}`} />
           <input type="email" id="email" value={email} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out" />
           <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
-            <p className='flex items-center'>Do you want to chang your name?<span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>Edit</span></p>
+            <p className='flex items-center'>Do you want to chang your name?<span onClick={() => { changDetail && onSubmit(); setChangDetail((preStatus)=> !preStatus);}} className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>{changDetail ? "Apply change" : "Edit"}</span></p>
             <p onClick={onLogout} className='text-blue-600 hover:text-blue-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>Sign Out?</p>
           </div>
 
